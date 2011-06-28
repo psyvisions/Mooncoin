922d1a3bb7a7cabfad34e0d5d3f0a605342a211e
mrmoon <mrmooncoin@gmail.com>
2011-06-28 10:30:23 -0700

    First steps of namecoin support. Generated with modified Bitcoin::Model created by pavel and modified Finance::Bitcoin module
    Signed-off-by: mrmoon <mrmooncoin@gmail.com>

---
+++ lib/Room/Model/NamecoinServer.pm
@@ -0,0 +1,35 @@
+package Room::Model::NamecoinServer;
+
+use strict;
+use warnings;
+
+use base qw/ Catalyst::Model::Namecoin /;
+
+__PACKAGE__->config(
+  uri => '',
+);
+
+=head1 NAME
+
+Room::Model::NamecoinServer - Namecoin Server Model Class
+
+=head1 SYNOPSIS
+
+See L<Room>.
+
+=head1 DESCRIPTION 
+
+Namecoin Server Model Class.
+
+=head1 AUTHOR
+
+open,,,
+
+=head1 LICENSE
+
+This module is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself. See L<perlartistic>.
+
+=cut
+
+1;
